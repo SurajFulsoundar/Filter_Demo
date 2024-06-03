6237d96ba3c47a560c0152ddd206791c59beb890
Suraj Fulsoundar <100521841+SurajFulsoundar@users.noreply.github.com>
2024-06-03 17:26:10 +0530

    Add project files.

---
+++ Filter_Demo/Models/UserLogAttribute.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Filter_Demo.Models
+{
+    public class UserLogAttribute : Attribute, IActionFilter , IResultFilter, IExceptionFilter
+    {
+        public void LogExecutionTime(string msg)
+        {
+            File.AppendAllText(Path.Combine(Environment.CurrentDirectory, @"Data\", "Data.txt"), msg);
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            string info = "After Action Execution -->"+ context.Controller.ToString() + "-->" + context.ActionDescriptor.DisplayName + "-->" + DateTime.Now.ToString();
+             LogExecutionTime(info);
+            LogExecutionTime("\n----------------------------\n");
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            string info = "Before Action Execution -->" + context.Controller.ToString() + "-->" + context.ActionDescriptor.DisplayName + "-->" + DateTime.Now.ToString();
+            LogExecutionTime(info);
+            LogExecutionTime("\n----------------------------\n");
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            string info = "On Exception -->" + context.Exception.ToString() + "-->" + context.ActionDescriptor.DisplayName + "-->" + DateTime.Now.ToString();
+            LogExecutionTime(info);
+            LogExecutionTime("\n----------------------------\n");
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            string info = "After Result Execution -->" + context.Controller.ToString() + "-->" + context.ActionDescriptor.DisplayName + "-->" + DateTime.Now.ToString();
+            LogExecutionTime(info);
+            LogExecutionTime("\n----------------------------\n");
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            string info = "Before Result Execution -->" + context.Controller.ToString() + "-->" + context.ActionDescriptor.DisplayName + "-->" + DateTime.Now.ToString();
+            LogExecutionTime(info);
+            LogExecutionTime("\n----------------------------\n");
+        }
+    }
+}
